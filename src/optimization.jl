3f06ff0549b3dd1d74df1c04638275deb3ddfb21
David Lung <lungdm@gmail.com>
2021-07-03 19:09:43 +0200

    Flux.chain compatibility (WIP)

--- src/optimization.jl
+++ src/optimization.jl
@@ -17,3 +17,23 @@ function optimize(model, loss, cb, opt, AD, train_dl)
                                 )
   GalacticOptim.solve(optprob, opt, train_dl, cb = cb)
 end
+
+
+function optimize(model::Flux.Chain, loss, cb, opt, AD, train_dl)
+  lb, ub = get_bounds(model)
+
+  @show length(train_dl)
+  @show size(first(train_dl)[1])
+  @show size(first(train_dl)[1][1])
+  @show length(pp)
+  @show length(lb)
+  @show length(ub)
+
+  optfun = GalacticOptim.OptimizationFunction((θ, p, x, y) -> loss(p,re, x,y), AD)
+  optfunc = GalacticOptim.instantiate_function(optfun, pp, AD, nothing)
+  optprob = GalacticOptim.OptimizationProblem(optfunc, pp, lb=lb, ub=ub,
+                                #grad = true, hess = true, sparse = true,
+                                #parallel=ModelingToolkit.MultithreadedForm()
+                                )
+  GalacticOptim.solve(optprob, opt, train_dl, cb = cb)
+end
